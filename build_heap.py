8524f25ee9a447012365093ea6bd2347f924367b
221RDC047 <93982525+jekabs247@users.noreply.github.com>
2023-03-19 16:42:19 +0000

    Test1

--- build_heap.py
+++ build_heap.py
@@ -6,9 +6,33 @@ def build_heap(data):
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
+    n = len(data)
+    for i in range(n // 2, -1, -1):
+        sift_down(i, data, swaps)
 
     return swaps
 
+def sift_down(i, data, swaps):
+
+    # sifts the element at index i down the heap
+    # until the min-heap property is satisfied
+
+    n = len(data)
+    min_index = i
+    left_child = 2 * i + 1
+
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+
+    right_child = 2 * i + 2
+
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        sift_down(min_index, data, swaps)
 
 def main():
     
@@ -20,6 +44,21 @@ def main():
     # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
+
+    temp = input()
+
+    if temp == "I":
+        n = int(input())
+        data = list(map(int, input().split()))
+
+    elif temp == "F":
+        fname = input()
+        if fname != "a":
+            f = open("./tests/"+fname, "r")
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+            f.close()
+
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
